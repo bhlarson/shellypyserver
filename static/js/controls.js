4d7abd41b857bc33f507e4bddae552c23aab0022
Brad Larson <bhlarson@gmail.com>
2023-02-24 09:34:57 -0800

    restore Control method

--- static/js/controls.js
+++ static/js/controls.js
@@ -24,11 +24,31 @@ function Devices() {
 // relay/0?turn=on 
 // relay/0?turn=on&timer=10 
 // relay/0?turn=toggle 
-function State(device='christmas') {
+function Control(device='christmas', action='relay/0?turn=toggle') {
 
     const params = {
         device: device,
         action: action
+    };
+    const options = {
+        method: 'PUT',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify( params )  
+    };
+
+    fetch('/command', options)
+    .then(function (response) {
+        return response.json();
+    }).then(function (status) {
+        console.log('PUT response:');
+        console.log(status); 
+    });
+ }
+
+function State(device='christmas') {
+
+    const params = {
+        device: device
     };
     const options = {
         method: 'GET',
