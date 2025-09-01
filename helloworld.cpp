15c15f5b9c56a1ee32ee2af3cb62ee7c73eee9ea
bhlarson <bhlarson@gmail.com>
2025-08-31 20:50:39 -0700

    julia learning to debug

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,10 +7,22 @@ using namespace std;
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<int> nums {43, 26, 31415, 271828};
+
+    int nums_array[] = {43, 26, 31415, 271828};
+
+    cout << nums_array[0] << endl;
 
     for (const string& word : msg)
     {
         cout << word << " ";
+    }
+
+    cout << endl;
+
+    for (const int& num : nums)
+    {
+        cout << num << " ";
     }
     cout << endl;
 
